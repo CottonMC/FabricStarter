f711b63768c2a3c220341143655e977732237d4a
Meredith Espinosa <goclonefilms@gmail.com>
2022-09-02 15:41:09 -0700

    update after forever

--- src/main/java/io/github/cottonmc/starter/FabricStarter.java
+++ src/main/java/io/github/cottonmc/starter/FabricStarter.java
@@ -1,15 +1,16 @@
 package io.github.cottonmc.starter;
 
-import net.fabricmc.api.ModInitializer;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FabricStarter implements ModInitializer {
 	public static final String MODID = "starter";
-	public static final Logger LOGGER = LogManager.getLogger(MODID);
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
 	@Override
-	public void onInitialize() {
+	public void onInitialize(ModContainer mod) {
 
 	}
 }
